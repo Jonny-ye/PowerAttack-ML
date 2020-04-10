572311d5eb35f1206b77f77244ca81591f8cd001
Jonny-ye <jonny_yecy@163.com>
2020-04-10 09:45:16 +0800

    v1.0

---
+++ Client/src/Client.java
@@ -0,0 +1,88 @@
+import java.net.Socket;
+import java.util.Scanner;
+import java.io.PrintStream;
+import java.io.FileOutputStream;
+import java.io.File;
+import java.io.IOException;
+
+ public class  Client{
+    Socket client;
+    Scanner in;
+    PrintStream out;
+    public Client(){}
+    public void connect(String ip, int port)throws Exception{
+        this.client = new Socket(ip, port);
+        this.in = new Scanner(client.getInputStream());
+        this.in.useDelimiter(";");
+        this.out = new PrintStream(client.getOutputStream());
+        System.out.println("已连接服务器，IP地址：" + this.client.getInetAddress() + "\n");
+    }
+    public void close() throws Exception{
+        this.out.println("exit");          //发送关闭连接指令
+        this.client.shutdownInput();
+        this.client.shutdownOutput();
+        this.client.close();
+        System.out.println("服务器断开连接");
+    }
+    public void receive(int time)throws Exception{       //进行一次检测过程(检测数据行数)
+        File file = new File("./test_data/base.tmp");   //打开缓存文件
+        if(!file.getParentFile().exists()){
+            file.getParentFile().mkdirs();
+        }
+        if(!file.exists()){
+            file.createNewFile();
+        }
+        PrintStream writefile = new PrintStream(new FileOutputStream(file));
+        this.out.println(time);                  //发送要接收的日志的时间间隔
+        if(this.in.hasNext()){                   //阻塞等待服务器发送数据
+            writefile.println(this.in.next());   //输出到缓存文件
+        }
+    }
+    
+    //机器学习模型分析数据
+    public String analyzeData()throws Exception{
+        String cmd = "./src/test_pa.py";
+        Runtime runtime = Runtime.getRuntime();
+        Process process = runtime.exec(cmd);
+        if(process.waitFor() != 0){
+            System.out.println("数据计算失败");
+            return null;
+        }
+        
+        Scanner scan = new Scanner(process.getInputStream());
+        scan.useDelimiter("\n");
+        StringBuffer buf = new StringBuffer();
+        while(scan.hasNext()){
+            buf.append(scan.next());
+        }
+        scan.close();
+        return buf.toString();
+    }
+    
+    public void timer(int s)throws Exception{
+        while((s--) > 0){
+            System.out.printf("等待服务器搜集日志信息:%2ds\r", s);
+            Thread.sleep(1000);
+        }
+        System.out.println("\n");
+    }
+    
+    public static void main(String args[]){
+        Client pac = new Client();
+        try{
+            pac.connect("localhost",5973);
+            pac.timer(30);
+            pac.receive(10);
+            System.out.println(pac.analyzeData());
+        }catch(Exception e){
+            System.out.println(e);
+        }finally{
+            try{
+                pac.close();
+            }catch(Exception ex){
+                System.out.println(ex);
+            }
+        }
+    }
+}
+ 
--- Client/src/ClientG.java
+++ Client/src/ClientG.java
@@ -10,88 +10,8 @@ import java.io.FileOutputStream;
 import java.io.File;
 import java.io.IOException;
 
- class PA_Client{
-    Socket client;
-    Scanner in;
-    PrintStream out;
-    public PA_Client(){}
-    public void connect(String ip, int port)throws Exception{
-        this.client = new Socket(ip, port);
-        this.in = new Scanner(client.getInputStream());
-        this.in.useDelimiter(";");
-        this.out = new PrintStream(client.getOutputStream());
-        System.out.println("已连接服务器，IP地址：" + this.client.getInetAddress() + "\n");
-    }
-    public void close() throws Exception{
-        this.out.println("exit");          //发送关闭连接指令
-        this.client.shutdownInput();
-        this.client.shutdownOutput();
-        this.client.close();
-        System.out.println("服务器断开连接");
-    }
-    public void receive(int time)throws Exception{       //进行一次检测过程(检测数据行数)
-        File file = new File("./test_data/base.tmp");   //打开缓存文件
-        if(!file.getParentFile().exists()){
-            file.getParentFile().mkdirs();
-        }
-        if(!file.exists()){
-            file.createNewFile();
-        }
-        PrintStream writefile = new PrintStream(new FileOutputStream(file));
-        this.out.println(time);                  //发送要接收的日志的时间间隔
-        if(this.in.hasNext()){                   //阻塞等待服务器发送数据
-            writefile.println(this.in.next());   //输出到缓存文件
-        }
-    }
-    
-    //机器学习模型分析数据
-    public String analyzeData()throws Exception{
-        String cmd = "./src/test_pa.py";
-        Runtime runtime = Runtime.getRuntime();
-        Process process = runtime.exec(cmd);
-        if(process.waitFor() != 0){
-            System.out.println("数据计算失败");
-            return null;
-        }
-        
-        Scanner scan = new Scanner(process.getInputStream());
-        scan.useDelimiter("\n");
-        StringBuffer buf = new StringBuffer();
-        while(scan.hasNext()){
-            buf.append(scan.next());
-        }
-        scan.close();
-        return buf.toString();
-    }
-    
-    public void timer(int s)throws Exception{
-        while((s--) > 0){
-            System.out.printf("等待服务器搜集日志信息:%2ds\r", s);
-            Thread.sleep(1000);
-        }
-        System.out.println("\n");
-    }
-    
-    public static void main(String args[]){
-        PA_Client pac = new PA_Client();
-        try{
-            pac.connect("localhost",5973);
-            pac.timer(30);
-            pac.receive(10);
-            System.out.println(pac.analyzeData());
-        }catch(Exception e){
-            System.out.println(e);
-        }finally{
-            try{
-                pac.close();
-            }catch(Exception ex){
-                System.out.println(ex);
-            }
-        }
-    }
-}
 
-public class ClientSwing {
+public class ClientG{
     private JFrame frame;
     private JFrame jf;  //弹窗
     private JMenuBar menuBar;
@@ -111,10 +31,10 @@ public class ClientSwing {
     private JTextField cntText;
     private JButton startButton;
     private JButton connectButton;
-    private int connect_status;
-    private int start_status;
+    private int connect_status; //链接状态
+    private int start_status;   //检测状态
     private int countRun;
-    private PA_Client client;
+    private Client client;
     private Runnable startRun = new Runnable(){ 
         public void run(){
             try{
@@ -229,7 +149,7 @@ public class ClientSwing {
             }
         }
     };
-    private ClientSwing(){
+    private ClientG(){
         this.frame = new JFrame("服务器电力攻击检测客户端");
         this.panel = new JPanel(null);
         this.jf = new JFrame();
@@ -246,7 +166,7 @@ public class ClientSwing {
         this.msgLabel = new JLabel("( 0 / 5 )");
         this.startButton = new JButton("开始");
         this.connectButton = new JButton("连接");
-        this.client = new PA_Client();
+        this.client = new Client();
         this.connect_status = 0;
         this.start_status = 0;
         this.countRun = 0;
@@ -290,7 +210,14 @@ public class ClientSwing {
         openMenuItem.addActionListener(new ActionListener(){
             public void actionPerformed(ActionEvent e){
                 try{
-                    Desktop.getDesktop().open(new File("./test_data/history.tmp"));
+                    File file = new File("./test_data/history.tmp");
+                    if(!file.getParentFile().exists()){
+                        file.getParentFile().mkdirs();
+                    }
+                    if(!file.exists()){
+                        file.createNewFile();
+                    }
+                    Desktop.getDesktop().open(file);
                 }catch(IOException ioe){
                     System.out.println(ioe);
                 }
@@ -489,6 +416,6 @@ public class ClientSwing {
         this.frame.setVisible(true);
     }
     public static void main(String[] args) {    
-        new ClientSwing().init();
+        new ClientG().init();
     }
 }
